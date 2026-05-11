3a340a7e510099c604dbcc4cbbdcfe7e54cef873
Joel <joel@Joels-MacBook-Pro.local>
2026-05-11 09:14:06 +0530

    slicea' a' ' '

--- GO/test.go
+++ GO/test.go
@@ -2,29 +2,28 @@ package main
 
 import "fmt"
 
-type reader interface {
-	read(string)
-}
-
-type book struct {
-	author    string
-	title     string
-	publisher string
-}
-
-func (b *book) read(s string) {
-	fmt.Println(s)
-}
-
-type record struct {
-	album  string
-	year   int
-	artist string
-}
-
-func (r *record) read(s string) {
-	fmt.Println(s)
+type stack struct {
+	val int
 }
 
 func main() {
+
+	var n int
+	fmt.Println("How many numbers do you want in the slice?")
+	fmt.Scan(&n)
+	push(n)
+
+}
+
+func push(n int) {
+
+	var slic []int
+	var num int
+	for i := 1; i <= n; i++ {
+		fmt.Printf("Enter the %d number to append : ", i)
+		fmt.Scan(&num)
+		slic = append(slic, num)
+	}
+
+	fmt.Println(slic)
 }
